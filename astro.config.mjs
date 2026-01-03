f2ed58a7c4e922025da4b0cb675a21dfdd8a2b06
yigit <kilicygt@hotmail.com>
2026-01-03 21:53:56 +0300

    İlk sürüm tamamlandı

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
-    output: 'server',
-    adapter: node({
-        mode: 'standalone'
-    })
+  // Sitenin gerçek adresi
+  site: 'https://izmiruzmani.com',
+  
+  // GitHub Pages sunucu çalıştıramadığı için server modunu kaldırdık.
+  // Varsayılan olarak "static" moda dönecektir, bu da tam istediğimiz şey.
+  integrations: [tailwind()],
 });
