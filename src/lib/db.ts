3a0865f1719120713a16c3b39871c01257fdd088
yigit <kilicygt@hotmail.com>
2026-01-04 13:27:01 +0300

    db export fix 2

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,1 +1,10 @@
-export const db = {};
+const db = {
+  prepare: () => ({
+    all: () => [],
+    get: () => null,
+    run: () => null
+  })
+};
+
+// BURASI ÇOK ÖNEMLİ: "default" olarak dışarı aktarıyoruz
+export default db;
